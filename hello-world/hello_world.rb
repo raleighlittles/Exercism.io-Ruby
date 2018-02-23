9cea7ed3925ace36decb5251e032525bef718834
Raleigh Littles <raleighlittles@gmail.com>
2018-02-22 19:18:57 -0800

    First commit. Completed the Hello world exercise

---
+++ hello-world/hello_world.rb
@@ -0,0 +1,9 @@
+module HelloWorld
+# You can use a class here too, but a module is a bit more appropriate
+# since modules are typically for methods, and classes are more about objects
+
+  def self.hello
+    return 'Hello, World!'
+  end
+
+end
